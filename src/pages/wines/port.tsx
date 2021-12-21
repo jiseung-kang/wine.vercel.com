fb2883900afc0413b6e22fc488e97218ab4ce113
jiseung-kang <ill0o0lli@ajou.ac.kr>
2021-12-22 00:04:13 +0900

    ♻️  Refactoring Components

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,33 +1,25 @@
 import type { NextPage } from 'next';
-import axios from 'axios';
-import useSWR from 'swr';
-import { Wine } from '../../types/Wine';
-import { fetcher } from '../../utils/fetcher';
+import { Error, Loading, WineCard } from '../../components';
 import { useWineData } from '../../hooks/useWineData';
+import { Wine } from '../../types/Wine';
 
-const Port: NextPage = () => {
-	const { data, error } = useWineData('port');
+const PortWinePage: NextPage = () => {
+	const name = 'port';
+	const { data, error } = useWineData(name);
 
-	if (error) return <div>Failed to Loading...</div>;
-	if (!data) return <div>...Loading</div>;
+	if (error) return <Error />;
+	if (!data) return <Loading />;
 
 	return (
 		<div>
 			<h1>Wine</h1>
 			<main>
 				{data.map((wineData: Wine) => {
-					const { id, wine, winery } = wineData;
-
-					return (
-						<div key={`port-wine-list-$(wineData.id)`}>
-							<h1>{wineData.wine}</h1>
-							<p>{wineData.winery}</p>
-						</div>
-					);
+					return <WineCard key={`port-wine-list-${wineData.id}`} wineData={wineData} />;
 				})}
 			</main>
 		</div>
 	);
 };
 
-export default Port;
+export default PortWinePage;
--- src/pages/wines/reds.tsx
+++ src/pages/wines/reds.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next';
+import { Error, Loading, WineCard } from '../../components';
+import { useWineData } from '../../hooks/useWineData';
+import { Wine } from '../../types/Wine';
 
-const Red: NextPage = () => {
+const RedWinePage: NextPage = () => {
+	const name = 'reds';
+	const { data, error } = useWineData(name);
+
+	if (error) return <Error />;
+	if (!data) return <Loading />;
+
 	return (
 		<div>
-			<h1>Reds</h1>
+			<h1>Wine</h1>
+			<main>
+				{data.map((wineData: Wine) => {
+					return <WineCard key={`red-wine-list-${wineData.id}`} wineData={wineData} />;
+				})}
+			</main>
 		</div>
 	);
 };
 
-export default Red;
+export default RedWinePage;
