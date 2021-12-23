a1be6b36514897f889d2f57100bd02659afa7421
jiseung-kang <ill0o0lli@ajou.ac.kr>
2021-12-23 19:19:21 +0900

    ➕ add

--- src/pages/test_style.tsx
+++ src/pages/test_style.tsx
@@ -1,19 +1,49 @@
 import type { NextPage } from 'next';
 import { Fragment } from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+		background-color: green;
+  }
+  * {
+    font-family: 'Open Sans', sans-serif;
+  }`;
 
 const TestPage: NextPage = () => {
 	return (
-		<Fragment>
-			<Button />
-		</Fragment>
+		<>
+			<GlobalStyle />
+			<Container>
+				<Button />
+				<Button />
+				<Anchor href="#">go google</Anchor>
+				<Test href="http://www.google.com">go google</Test>
+			</Container>
+		</>
 	);
 };
+
+const Container = styled.div`
+	height: 100vh;
+	width: 50%;
+	background-color: pink;
+`;
 
 const Button = styled.button`
 	width: 100px;
 	background-color: blue;
 	height: 100px;
+	color: white;
 `;
+
+const Anchor = styled(Button)`
+	background-color: yellow;
+`.withComponent('a');
+
+const Test = Button.withComponent('a');
 
 export default TestPage;
