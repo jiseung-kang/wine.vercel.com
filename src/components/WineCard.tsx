a7ff94bc9806fe79c4b7e788838caee80460a5c5
jiseung-kang <ill0o0lli@ajou.ac.kr>
2021-12-23 20:07:52 +0900

    💄  use Styled-Components

--- src/components/WineCard.tsx
+++ src/components/WineCard.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { Wine } from '../types/Wine';
 
 interface WineProps {
@@ -5,12 +6,48 @@ interface WineProps {
 }
 
 export const WineCard = ({ wineData }: WineProps) => {
-	const { wine, winery } = wineData;
+	const { wine, winery, image, location, rating } = wineData;
 
 	return (
-		<div>
-			<h1>{wine}</h1>
-			<p>{winery}</p>
-		</div>
+		<Container>
+			<img src={image} alt="" />
+			<Title>
+				{wine}
+				<Average>{rating.average}</Average>
+			</Title>
+			<p>
+				{winery}
+				<span>{location}</span>
+			</p>
+			<p>별점 : {rating.reviews.replace(' ratings', '')}</p>
+		</Container>
 	);
 };
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-end;
+	padding: 1em;
+	box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+	border-radius: 1em;
+`;
+
+const Title = styled.h2`
+	text-align: center;
+`;
+
+const ImageContainer = styled.div`
+	width: 100%;
+`;
+
+const Average = styled.span`
+	display: inline-block;
+	padding: 0.3em;
+	font-size: 16px;
+	margin-left: 0.5em;
+	color: white;
+	backgroung-color: #2acbc;
+	border-radius: 3px;
+`;
